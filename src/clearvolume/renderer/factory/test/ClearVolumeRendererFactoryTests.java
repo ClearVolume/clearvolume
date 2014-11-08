461b176f67e6d00a1f8dbdb6923f60983165f3c2
Loic Alain Royer <royerloic@gmail.com>
2014-11-08 16:15:54 +0100

    fixed renderer factory

--- src/clearvolume/renderer/factory/test/ClearVolumeRendererFactoryTests.java
+++ src/clearvolume/renderer/factory/test/ClearVolumeRendererFactoryTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import clearvolume.renderer.ClearVolumeRendererInterface;
 import clearvolume.renderer.factory.ClearVolumeRendererFactory;
 
 public class ClearVolumeRendererFactoryTests
@@ -15,9 +16,11 @@ public class ClearVolumeRendererFactoryTests
 	{
 		try
 		{
-			assertNotNull(ClearVolumeRendererFactory.newBestRenderer(	"Test",
-																																128,
-																																128));
+			ClearVolumeRendererInterface lNewBestRenderer = ClearVolumeRendererFactory.newBestRenderer(	"Test",
+																																																	128,
+																																																	128);
+			System.out.println(lNewBestRenderer.getClass());
+			assertNotNull(lNewBestRenderer);
 		}
 		catch (Throwable e)
 		{
