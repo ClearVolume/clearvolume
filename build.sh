e5d29d42b3ab235790d0ac7f5658725992dc5ddd
Loic Alain Royer <royerloic@gmail.com>
2014-12-05 19:02:42 +0100

    small fix

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ chmod +x ./packagedmg.sh
 chmod +x ./packagezip.sh
 chmod +x ./client.sh
 chmod +x packagewinapp.sh
+chmod +x ./deploy/osx/ClearVolume.app/Contents/MacOS/ClearVolume
 gradle cleanEclipse eclipse build FatCapsule copyCapsule packagecvlib packagelvcvlib
