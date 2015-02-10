b6132c3990b8497f6d5faa492e35c01aa5020175
Martin Weigert <mweigert@mpi-cbg.de>
2015-02-10 18:54:47 +0100

    OpenCL processor for center of mass

---
+++ src/java/clearvolume/renderer/processors/impl/OpenCLCenterMass.java
@@ -0,0 +1,103 @@
+package clearvolume.renderer.processors.impl;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import clearvolume.renderer.processors.OpenCLProcessor;
+
+import com.nativelibs4java.opencl.CLBuffer;
+import com.nativelibs4java.opencl.CLKernel;
+
+public class OpenCLCenterMass extends OpenCLProcessor<IntBuffer>
+{
+
+	private CLKernel mKernelHist;
+
+	private CLBuffer<Float> mBufBins;
+	private CLBuffer<Integer> mBufCounts;
+	private float[] mBins;
+
+	@Override
+	public String getName()
+	{
+		return "opencl_histogram";
+	}
+
+	public void setBins(final float[] pBins)
+	{
+		mBins = pBins;
+
+	}
+
+	public void ensureOpenCLInitialized()
+	{
+		if (mKernelHist == null)
+		{
+			mKernelHist = getDevice().compileKernel(OpenCLCenterMass.class.getResource("kernels/histogram.cl"),
+																							"histogram");
+		}
+
+		float[] bins = new float[10];
+		for (int i = 0; i < bins.length; i++)
+		{
+			bins[i] = (float) (1.f * i / bins.length);
+		}
+		setBins(bins);
+
+	}
+
+	public void initBuffers()
+	{
+
+		int lBinSize = mBins.length;
+
+		// the buffer containing the counts
+		mBufCounts = getDevice().createOutputIntBuffer(lBinSize);
+
+		// the buffer containing the bins values
+		mBufBins = getDevice().createInputFloatBuffer(lBinSize);
+
+	}
+
+	@Override
+	public void process(int pRenderLayerIndex,
+											long pWidthInVoxels,
+											long pHeightInVoxels,
+											long pDepthInVoxels)
+	{
+
+		ensureOpenCLInitialized();
+
+		if (mBufBins == null)
+		{
+			System.out.println("setting up buffers");
+			initBuffers();
+		}
+
+		// fill the bins
+		getDevice().writeFloatBuffer(mBufBins, FloatBuffer.wrap(mBins));
+
+		FloatBuffer bins = FloatBuffer.wrap(mBins);
+
+		mKernelHist.setArgs(getVolumeBuffers()[0],
+												mBufBins,
+												mBufCounts,
+												(int) mBins.length);
+
+		getDevice().run(mKernelHist,
+										(int) pWidthInVoxels,
+										(int) pHeightInVoxels,
+										(int) pDepthInVoxels);
+
+		IntBuffer out = getDevice().readIntBufferAsByte(mBufCounts)
+																.asIntBuffer();
+
+		for (int i = 0; i < out.capacity(); i++)
+		{
+			System.out.println(out.get(i));
+		}
+
+		notifyListenersOfResult(out);
+
+	}
+}
---
+++ src/java/clearvolume/renderer/processors/impl/kernels/centermass.cl
@@ -0,0 +1,64 @@
+// center of mass calculations
+
+
+#define LSIZE 8
+
+// center of mass
+__kernel void center_of_mass_img(__read_only image3d_t input,
+							 __global float * rx,
+							 __global float * ry,
+							 __global float * rz,
+							 __global float * rm,							 
+							 const int Nx, const int Ny, const int Nz){
+
+  
+  const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE |	CLK_ADDRESS_CLAMP_TO_EDGE |	CLK_FILTER_NEAREST ;
+
+  
+  int i0 = get_global_id(0);
+  int j0 = get_global_id(1);
+  int k0 = get_global_id(2);
+
+  int iLoc = get_local_id(0);
+  int jLoc = get_local_id(1);
+  int kLoc = get_local_id(2);
+
+  int iGroup = get_group_id(0);
+  int jGroup = get_group_id(1);
+  int kGroup = get_group_id(2);
+
+  int iDim = get_num_groups(0);
+  int jDim = get_num_groups(1);
+  int kDim = get_num_groups(2);
+
+  __local float4 partialSums[LSIZE][LSIZE][LSIZE];
+
+  float val = read_imagef(input,sampler,(float4)(i0,j0,k0,0.f)).x;
+
+  int i = i0;
+  
+  partialSums[iLoc][jLoc][kLoc] = val*(float4)(i0,j0,k0,1.f);
+
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+  
+  //sum all up
+  
+  if (iLoc==0){
+
+	float4 res = 0.f;
+	for (int i2 = 0; i2 < LSIZE; ++i2)
+	  for (int j2 = 0; j2 < LSIZE; ++j2)
+		for (int k2 = 0; k2 < LSIZE; ++k2)
+		  res += partialSums[i2][j2][k2];
+	
+	rx[iGroup+jGroup*iDim+kGroup*iDim*jDim] = res.x;
+	ry[iGroup+jGroup*iDim+kGroup*iDim*jDim] = res.y;
+	rz[iGroup+jGroup*iDim+kGroup*iDim*jDim] = res.z;
+	rm[iGroup+jGroup*iDim+kGroup*iDim*jDim] = res.w;
+
+  }
+ 
+}
+
+
