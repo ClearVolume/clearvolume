3c45262f4b2294e5b6c6b48742e6ffa04a1f9a8a
Martin Weigert <mweigert@mpi-cbg.de>
2015-02-17 13:15:25 +0100

    minor changes in processors

--- src/java/clearvolume/renderer/processors/impl/OpenCLCenterMass.java
+++ src/java/clearvolume/renderer/processors/impl/OpenCLCenterMass.java
@@ -87,15 +87,6 @@ public class OpenCLCenterMass extends OpenCLProcessor<float[]> {
 		getDevice().run(mKernel, mPaddedShapeX, mPaddedShapeY, mPaddedShapeZ,
 				mLocalSize, mLocalSize, mLocalSize);
 
-		if (isdebug) {
-			getDevice().mCLQueue.finish();
-			long end = System.nanoTime();
-			System.out.println("time to compute center of mass " + 1.e-6
-					* (end - start) + " ms");
-			// System.out.printf("time to compute center of mass: %.2f ms\n",
-			// 1000000 * (start - end));
-		}
-
 		final FloatBuffer outX = getDevice().readFloatBuffer(mBufX);
 		final FloatBuffer outY = getDevice().readFloatBuffer(mBufY);
 		final FloatBuffer outZ = getDevice().readFloatBuffer(mBufZ);
@@ -109,6 +100,13 @@ public class OpenCLCenterMass extends OpenCLProcessor<float[]> {
 			resY += outY.get(i);
 			resZ += outZ.get(i);
 			resSum += outSum.get(i);
+		}
+
+		if (isdebug) {
+
+			long end = System.nanoTime();
+			System.out.println("time to compute center of mass " + 1.e-6
+					* (end - start) + " ms");
 		}
 
 		notifyListenersOfResult(new float[] { resX / resSum, resY / resSum,
--- src/java/clearvolume/renderer/processors/impl/kernels/tenengrad.cl
+++ src/java/clearvolume/renderer/processors/impl/kernels/tenengrad.cl
@@ -216,4 +216,4 @@ __kernel void copy(__global float * input, __global float * output , const int N
   int i = get_global_id(0);
   
   		output[i] = input[i];
-		}
+}
